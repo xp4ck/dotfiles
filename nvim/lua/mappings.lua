255800293990aa0f49862f34964eca00665c2448
Yegor <h3rmit@protonmail.com>
2023-07-23 14:30:09 +0300

    add new mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,6 +1,13 @@
 vim.g.mapleader = " "
 
+-- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+
+-- windows
+vim.keymap.set('n', '<C-h>', '<C-w>h', {})
+vim.keymap.set('n', '<C-l>', '<C-w>l', {})
+vim.keymap.set('n', '<C-j>', '<C-w>j', {})
+vim.keymap.set('n', '<C-k>', '<C-w>k', {})
