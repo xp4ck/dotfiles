5bcde64ac1e6b828076f198f601caf4074afcbc9
Yegor <h3rmit@protonmail.com>
2024-05-07 12:21:59 +0300

    update colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,25 +1,20 @@
--- require("modus-themes").setup({
---     style = "modus_vivendi",
---     dim_inactive = false,
--- })
--- require("solarized-osaka").setup({
---     transparent = true,
--- })
-require("nebulous").setup {
-    variant = "night",
-    disable = {
-        background = false
-    }
-}
--- vim.cmd.colorscheme "solarized-osaka"
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
--- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NvimTreeNC", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NvimTreeFloat", { bg = "none" })
--- vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg = "none" })
--- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
--- vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+require("modus-themes").setup({
+    style = "modus_operandi",
+    transparent = false,
+})
+-- require'meadow'.setup {
+--   color_saturation = 80, -- contrast (0-100)
+--   color_value = 75, -- brightness (0-100)
+-- }
+vim.cmd.colorscheme 'modus'
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NvimTreeFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
