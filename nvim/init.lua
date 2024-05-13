097adfda4604e5b6374be8cd7e36eeb7835fbae5
Yegor <h3rmit@protonmail.com>
2024-05-13 09:48:44 +0300

    Use lazy.nvim for plugin management

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,19 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
-require("mappings")
 require("plugins")
+require("mappings")
 require("set")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,14 +1,11 @@
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
+return require('lazy').setup({
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
     requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('tpope/vim-fugitive')
-  use {
+  },
+  'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'},
+  'tpope/vim-fugitive',
+  {
       'VonHeikemen/lsp-zero.nvim',
       branch = 'v2.x',
       requires = {
@@ -27,37 +24,37 @@ return require('packer').startup(function(use)
         {'hrsh7th/cmp-nvim-lsp'}, -- Required
         {'L3MON4D3/LuaSnip'},     -- Required
       }
-  }
-  use({ "miikanissi/modus-themes.nvim" })
-  use 'windwp/nvim-autopairs'
-  use 'terrortylor/nvim-comment'
-  use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
-  use "mfussenegger/nvim-dap"
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
-  use "mfussenegger/nvim-dap-python"
-  use "leoluz/nvim-dap-go"
-  use {
+  },
+  { "miikanissi/modus-themes.nvim" },
+  'windwp/nvim-autopairs',
+  'terrortylor/nvim-comment',
+  {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'},
+  "mfussenegger/nvim-dap",
+  { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+  "mfussenegger/nvim-dap-python",
+  "leoluz/nvim-dap-go",
+  {
       'nvim-lualine/lualine.nvim',
       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use "lewis6991/gitsigns.nvim"
-  use {
+  },
+  "lewis6991/gitsigns.nvim",
+  {
   "nvim-neotest/neotest",
     requires = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
         "antoinemadec/FixCursorHold.nvim"
     }
-  }
-  use {"nvim-neotest/neotest-python"}
-  use "fatih/vim-go"
-  use {
+  },
+  {"nvim-neotest/neotest-python"},
+  "fatih/vim-go",
+  {
     "nvim-telescope/telescope-file-browser.nvim",
     requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  }
-  use{'nvim-tree/nvim-tree.lua', requires = {{'nvim-tree/nvim-web-devicons'}}}
-  use {'nyoom-engineering/oxocarbon.nvim'}
-  use 'jacoborus/tender.vim'
-  use "craftzdog/solarized-osaka.nvim"
-  use 'Yagua/nebulous.nvim'
-end)
+  },
+  {'nvim-tree/nvim-tree.lua', requires = {{'nvim-tree/nvim-web-devicons'}}},
+  {'nyoom-engineering/oxocarbon.nvim'},
+  'jacoborus/tender.vim',
+  "craftzdog/solarized-osaka.nvim",
+  'Yagua/nebulous.nvim',
+  })
